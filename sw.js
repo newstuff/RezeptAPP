371de77a37fd8b5d916eec2f81eb0dd8bde7463f
Nic <nic.karl@me.com>
2023-12-27 21:33:13 +0100

    Initial commit

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', event => {
+    // Hier können Sie Ressourcen cachen
+});
+
+self.addEventListener('fetch', event => {
+    // Hier können Sie benutzerdefinierte Antworten auf Netzwerkanfragen implementieren
+});
